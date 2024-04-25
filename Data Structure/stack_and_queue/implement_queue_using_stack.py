7549893ac35cae33f97ef7f86ed7e9449a34ff10
Neha Pandey <I31430@verisk.com>
2024-04-25 20:08:53 +0545

    add stack questions/soluton

---
+++ Data Structure/stack_and_queue/implement_queue_using_stack.py
@@ -0,0 +1,27 @@
+# create two stacks
+# first insert the s1 elements to s2 element by element
+# push the new element in s1
+# and then keep all the element back to s1
+
+
+# optimize the performance
+# push(x)
+# add x-> input stack
+
+# pop()
+# if(output not empty):
+# output.pop()
+#else:
+# input stack-> output stack
+# output.pop()
+
+#top():
+# if(output not empty):
+# output.pop()
+#else:
+# input stack-> output stack
+# output.pop()
+
+
+
+
